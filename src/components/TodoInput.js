dd80d47a339eea025f24f8089b51a00efd369d95
kimtaekjin <qlcdmlrlf@naver.com>
2024-05-09 13:11:27 +0900

    react-todo-app 기본구조 및 scss , 할일 추가 form 이벤트 처리

---
+++ src/components/TodoInput.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import '../scss/TodoInput.scss';
+import { MdAdd } from 'react-icons/md';
+import cn from 'classnames'
+
+const TodoInput = () => {
+  const [open, setOpen] =useState(false);
+
+  // +버튼 클릭시 이벤트
+  const onToggle = () =>{
+    setOpen(!open);
+  }
+
+  return (
+    <>
+     { open && <div className='form-wrapper'>
+        <form className='insert-form'>
+          <input
+            type='text'
+            placeholder='할 일을 입력 후, 엔터를 누르세요!'
+          />
+        </form>
+      </div>}
+{/*
+          cn() : 첫번째 파라미터는 항상 유지할 default 클래스
+                 두번째 파라미터는 논리 상태값
+                 => 논리 상태값이 true일 경우 해당 클래스 추가
+                    false일 경우 제거.
+                    {클래스이름: 논리값}, 
+                    클래스 이름 지정 안할 시 변수명이 클래스 이름으로 사용됨.
+*/}
+      <button className={cn('insert-btn' , {open})} onClick={onToggle}>
+        <MdAdd />
+      </button>
+    </>
+  );
+};
+
+export default TodoInput;
