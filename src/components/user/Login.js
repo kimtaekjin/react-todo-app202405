d803c9d781f270b502bd3e84c7abd9dc8e49ad84
kimtaekjin <qlcdmlrlf@naver.com>
2024-05-27 10:47:35 +0900

    axios interceptor를 활용한 access token 갱신, TodoTemplate 리팩토링

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -5,20 +5,38 @@ import {
   TextField,
   Typography,
 } from '@mui/material';
-import React, { useContext } from 'react';
+import React, {
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import {
   API_BASE_URL as BASE,
   USER,
 } from '../../config/host-config';
 import AuthContext from '../../utils/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import CustomSnackBar from '../layout/CustomSnackBar';
+import { KAKAO_AUTH_URL } from '../../config/kakao-config';
+import axios from 'axios';
 
 const Login = () => {
   const REQUEST_URL = BASE + USER + '/signin';
 
-  const { onLogin } = useContext(AuthContext);
+  const { onLogin, isLoggedIn } = useContext(AuthContext);
+  const [open, setOpen] = useState(false);
 
   const redirection = useNavigate();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      setOpen(true); // 스낵바 오픈
+      // 일정 시간 뒤 Todo 화면으로 redirect
+      setTimeout(() => {
+        redirection('/');
+      }, 3500);
+    }
+  }, [isLoggedIn]);
 
   // 서버에 비동기 로그인 요청(AJAX 요청)
   // 함수 앞에 async를 붙이면 해당 함수는 프로미스 객체를 바로 리턴합니다.
@@ -31,6 +49,7 @@ const Login = () => {
     // await는 프로미스 객체가 처리될 때까지 기다립니다.
     // 프로미스 객체의 반환값을 바로 활용할 수 있도록 도와줍니다.
     // then()을 활용하는 것보다 가독성이 좋고, 쓰기도 쉽습니다.
+    /*
     const res = await fetch(REQUEST_URL, {
       method: 'POST',
       headers: { 'content-type': 'application/json' },
@@ -39,6 +58,11 @@ const Login = () => {
         password: $password.value,
       }),
     });
+    */
+    const res = await axios.post(REQUEST_URL, {
+      email: $email.value,
+      password: $password.value,
+    });
 
     if (res.status === 400) {
       const text = await res.text();
@@ -46,32 +70,13 @@ const Login = () => {
       return;
     }
 
-    const { token, userName, email, role } =
-      await res.json(); // 서버에서 전달된 json을 변수에 저장.
+    const { token, userName, role } = await res.data;
 
     // Context API를 사용하여 로그인 상태를 업데이트 합니다.
     onLogin(token, userName, role);
 
     // 홈으로 리다이렉트
     redirection('/');
-
-    /*
-    fetch(REQUEST_URL, {
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({
-        email: $email.value,
-        password: $password.value,
-      }),
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-      */
   };
 
   const loginHandler = (e) => {
@@ -84,57 +89,71 @@ const Login = () => {
   };
 
   return (
-    <Container
-      component='main'
-      maxWidth='xs'
-      style={{ margin: '200px auto' }}
-    >
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography component='h1' variant='h5'>
-            로그인
-          </Typography>
-        </Grid>
-      </Grid>
+    <>
+      {!isLoggedIn && (
+        <Container
+          component='main'
+          maxWidth='xs'
+          style={{ margin: '200px auto' }}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography component='h1' variant='h5'>
+                로그인
+              </Typography>
+            </Grid>
+          </Grid>
 
-      <form noValidate onSubmit={loginHandler}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              variant='outlined'
-              required
-              fullWidth
-              id='email'
-              label='email address'
-              name='email'
-              autoComplete='email'
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant='outlined'
-              required
-              fullWidth
-              name='password'
-              label='on your password'
-              type='password'
-              id='password'
-              autoComplete='current-password'
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              type='submit'
-              fullWidth
-              variant='contained'
-              color='primary'
-            >
-              로그인
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
-    </Container>
+          <form noValidate onSubmit={loginHandler}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  variant='outlined'
+                  required
+                  fullWidth
+                  id='email'
+                  label='email address'
+                  name='email'
+                  autoComplete='email'
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant='outlined'
+                  required
+                  fullWidth
+                  name='password'
+                  label='on your password'
+                  type='password'
+                  id='password'
+                  autoComplete='current-password'
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <Button
+                  type='submit'
+                  fullWidth
+                  variant='contained'
+                  color='primary'
+                >
+                  로그인
+                </Button>
+              </Grid>
+              <Grid item xs={12}>
+                <a href={KAKAO_AUTH_URL}>
+                  <img
+                    style={{ width: '100%' }}
+                    alt='kakaobtn'
+                    src={require('../../assets/img/kakao_login_medium_wide.png')}
+                  />
+                </a>
+              </Grid>
+            </Grid>
+          </form>
+        </Container>
+      )}
+      <CustomSnackBar open={open} />
+    </>
   );
 };
 
